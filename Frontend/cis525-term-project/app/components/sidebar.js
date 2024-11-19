ffe6bef6016e144537c6e4840d4e6ab18ad05aab
RockosBos <32206608+RockosBos@users.noreply.github.com>
2024-11-18 19:28:00 -0500

    Header and Sidebar Creation

---
+++ Frontend/cis525-term-project/app/components/sidebar.js
@@ -0,0 +1,22 @@
+"use client"
+
+import React, { Component } from 'react'
+import "./sidebar.css"
+
+
+export default class Sidebar extends Component{
+
+	render(){
+		return(
+			<div class="header">
+				<img src={null} alt="Logo" />
+				<div class="login">
+					<button onPress={""} class="login-item" id="new-account-button">Create a New Account</button>
+					<button onPress={""} class="login-item" id="login-button">Login</button>
+					<button onPress={""} class="login-item" id="admin-page-button">Admin</button>
+				</div>
+
+			</div>
+		)
+	}
+}
