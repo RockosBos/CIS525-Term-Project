7dd139e3e3e426d2a59c5c9aa663dcac556bbd78
RockosBos <32206608+RockosBos@users.noreply.github.com>
2024-12-15 20:45:11 -0500

    gh-pages

--- pokemon-immaculate-grid/next.config.mjs
+++ pokemon-immaculate-grid/next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	env: {
-		host_dev: 'localhost',
+		host_dev: 'database-1.crw8o4wc8c9o.us-east-1.rds.amazonaws.com',
 		port_dev: '3306',
-		user_dev: 'root',
-		password_dev: '',
-		database_dev: 'pokemon_grid',
+		user_dev: 'admin',
+		password_dev: 'h4bioKQrCquoAwwCPiV9',
+		database_dev: 'pokemon-grid',
 
-		host: 'localhost',
+		host: 'database-1.crw8o4wc8c9o.us-east-1.rds.amazonaws.com',
 		port: '3306',
-		user: 'root',
-		password: '',
-		database: 'pokemon_grid',
+		user: 'admin',
+		password: 'h4bioKQrCquoAwwCPiV9',
+		database: 'pokemon-grid',
 	},
 	reactStrictMode: false,
 };
